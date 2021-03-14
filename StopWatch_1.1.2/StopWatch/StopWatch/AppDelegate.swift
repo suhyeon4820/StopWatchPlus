217529cfd5f14c2b37bb470d857283c439c0991c
suhyeon4820 <bananacco@naver.com>
2021-03-15 00:32:29 +0900

    21/03/15

---
+++ StopWatch_1.1.2/StopWatch/StopWatch/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  StopWatch
+//
+//  Created by 정수현 on 2020/11/29.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
